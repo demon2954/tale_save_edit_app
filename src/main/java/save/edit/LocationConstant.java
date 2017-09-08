49b33391df1e0abbc7b41a75ce6f980f30df7aef
chenzecong <czc@lizikj.com>
2017-09-08 18:13:03 +0800

    add some code

--- src/main/java/save/edit/LocationConstant.java
+++ src/main/java/save/edit/LocationConstant.java
@@ -6,6 +6,31 @@ package save.edit;
  *
  */
 public class LocationConstant {
+	/**	窗口宽 */
 	public final static int FRAME_SIZE_WIDTH = 640;
+	/**	窗口高 */
 	public final static int FRAME_SIZE_HEIGHT = 480;
+	
+	/** 选择文件位置x */
+	public final static int FILE_CHOOSER_SIZE_X = 1;
+	/** 选择文件位置y */
+	public final static int FILE_CHOOSER_SIZE_Y = 1;
+	/** 选择文件宽 */
+	public final static int FILE_CHOOSER_SIZE_WIDTH = 500;
+	/** 选择文件高 */
+	public final static int FILE_CHOOSER_SIZE_HEIGHT = 500;
+	
+
+	
+	/** 选择文件文本框位置x */
+	public final static int FILE_CHOOSER_BAR_SIZE_X = 1;
+	/** 选择文件文本框位置y */
+	public final static int FILE_CHOOSER_BAR_SIZE_Y = 1;
+	/** 选择文件文本框宽 */
+	public final static int FILE_CHOOSER_BAR_SIZE_WIDTH = 500;
+	/** 选择文件文本框高 */
+	public final static int FILE_CHOOSER_BAR_SIZE_HEIGHT = 30;
+	
+	
+	
 }
--- src/main/java/save/edit/ui/EditFrame.java
+++ src/main/java/save/edit/ui/EditFrame.java
@@ -2,9 +2,12 @@ package save.edit.ui;
 
 import java.awt.Dimension;
 import java.awt.Toolkit;
+import java.io.File;
 
-import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 import save.edit.LocationConstant;
 
@@ -19,12 +22,30 @@ public class EditFrame extends JFrame {
 		this.setBounds(x, y, LocationConstant.FRAME_SIZE_WIDTH, LocationConstant.FRAME_SIZE_HEIGHT);
 		this.setVisible(true);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		this.setResizable(false);
 		this.setLayout(null);
 
-		JButton btn = new JButton("确定");
-		btn.setBounds(1, 0, 122, 111);
-		this.add(btn);
+		JTextField fileChooserBar = new JTextField();
+		fileChooserBar.setBounds(LocationConstant.FILE_CHOOSER_BAR_SIZE_X, LocationConstant.FILE_CHOOSER_BAR_SIZE_Y,
+				LocationConstant.FILE_CHOOSER_BAR_SIZE_WIDTH, LocationConstant.FILE_CHOOSER_BAR_SIZE_HEIGHT);
+		this.add(fileChooserBar);
+		
+		JFileChooser fileChoose = new JFileChooser();
+		fileChoose.setBounds(LocationConstant.FILE_CHOOSER_SIZE_X, LocationConstant.FILE_CHOOSER_SIZE_Y,
+				LocationConstant.FILE_CHOOSER_SIZE_WIDTH, LocationConstant.FILE_CHOOSER_SIZE_HEIGHT);
+		fileChoose.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+		fileChoose.showDialog(new JLabel(), "选择存档文件");
+		File file = fileChoose.getSelectedFile();
+		if (file.isDirectory()) {
+			System.out.println("文件夹:" + file.getAbsolutePath());
+		} else if (file.isFile()) {
+			System.out.println("文件:" + file.getAbsolutePath());
+		}
+		fileChooserBar.setText(file.getAbsolutePath());
+		
+		
+//		this.add(fileChoose);
+		this.setResizable(false);
+
 	}
 
 	public static void main(String[] args) {
