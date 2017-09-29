848c8bd8a13be34bbe7ee1ccb36b57c6d17e7ba5
demon2954 <295528720@qq.com>
2017-09-29 16:42:19 +0800

    尝试采用配置文件方式记录功法

---
+++ src/main/java/save/edit/data/SkillDetail.java
@@ -0,0 +1,112 @@
+package save.edit.data;
+
+/**
+ * @auth zone
+ * @date 2017-09-29
+ */
+public class SkillDetail {
+	// 技艺书部分
+	private int m_iType;
+	private int m_iID;
+	private int m_iAbilityType;
+	private String m_strAbilityID;
+	private String m_strBookMsg;
+	private String m_strBookImage;
+	private String m_sValueLink;
+
+	// 非心法类
+	private int m_iRoutineID;
+	private String m_strRoutineName;
+	private int m_iWearAmsType;
+
+	// 心法类
+	private int m_iNeigongID;
+	private String m_strNeigongName;
+	private String m_strStatusImage;
+	private String m_strSelectImage;
+	
+	public int getM_iType() {
+		return m_iType;
+	}
+	public void setM_iType(int m_iType) {
+		this.m_iType = m_iType;
+	}
+	public int getM_iID() {
+		return m_iID;
+	}
+	public void setM_iID(int m_iID) {
+		this.m_iID = m_iID;
+	}
+	public int getM_iAbilityType() {
+		return m_iAbilityType;
+	}
+	public void setM_iAbilityType(int m_iAbilityType) {
+		this.m_iAbilityType = m_iAbilityType;
+	}
+	public String getM_strAbilityID() {
+		return m_strAbilityID;
+	}
+	public void setM_strAbilityID(String m_strAbilityID) {
+		this.m_strAbilityID = m_strAbilityID;
+	}
+	public String getM_strBookMsg() {
+		return m_strBookMsg;
+	}
+	public void setM_strBookMsg(String m_strBookMsg) {
+		this.m_strBookMsg = m_strBookMsg;
+	}
+	public String getM_strBookImage() {
+		return m_strBookImage;
+	}
+	public void setM_strBookImage(String m_strBookImage) {
+		this.m_strBookImage = m_strBookImage;
+	}
+	public String getM_sValueLink() {
+		return m_sValueLink;
+	}
+	public void setM_sValueLink(String m_sValueLink) {
+		this.m_sValueLink = m_sValueLink;
+	}
+	public int getM_iRoutineID() {
+		return m_iRoutineID;
+	}
+	public void setM_iRoutineID(int m_iRoutineID) {
+		this.m_iRoutineID = m_iRoutineID;
+	}
+	public String getM_strRoutineName() {
+		return m_strRoutineName;
+	}
+	public void setM_strRoutineName(String m_strRoutineName) {
+		this.m_strRoutineName = m_strRoutineName;
+	}
+	public int getM_iWearAmsType() {
+		return m_iWearAmsType;
+	}
+	public void setM_iWearAmsType(int m_iWearAmsType) {
+		this.m_iWearAmsType = m_iWearAmsType;
+	}
+	public int getM_iNeigongID() {
+		return m_iNeigongID;
+	}
+	public void setM_iNeigongID(int m_iNeigongID) {
+		this.m_iNeigongID = m_iNeigongID;
+	}
+	public String getM_strNeigongName() {
+		return m_strNeigongName;
+	}
+	public void setM_strNeigongName(String m_strNeigongName) {
+		this.m_strNeigongName = m_strNeigongName;
+	}
+	public String getM_strStatusImage() {
+		return m_strStatusImage;
+	}
+	public void setM_strStatusImage(String m_strStatusImage) {
+		this.m_strStatusImage = m_strStatusImage;
+	}
+	public String getM_strSelectImage() {
+		return m_strSelectImage;
+	}
+	public void setM_strSelectImage(String m_strSelectImage) {
+		this.m_strSelectImage = m_strSelectImage;
+	}
+}
---
+++ src/main/java/save/edit/data/SkillManager.java
@@ -0,0 +1,126 @@
+package save.edit.data;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+/**
+ * @auth zone
+ * @date 2017-09-29
+ */
+public class SkillManager {
+	private static int SKILL_TYPE_XINFA = 1;
+	private static int SKILL_TYPE_QUANZHANG = 2;
+	private static int SKILL_TYPE_ZHIFA = 3;
+	private static int SKILL_TYPE_JIANFA = 4;
+	private static int SKILL_TYPE_DAOFA = 5;
+	private static int SKILL_TYPE_TUIFA = 6;
+	
+	private static Properties namePro = null;
+	private static Properties detailPro = null;
+	private static Properties bookPro = null;
+	
+	private static Map<Integer, Map<String, SkillDetail>> skillMap = new HashMap<Integer, Map<String,SkillDetail>>();
+	
+	static {
+		loadProperty();
+		init();
+	}
+	
+	private static void loadProperty() {
+		try {
+			namePro = new Properties();
+			InputStream in = ClassLoader.class.getResourceAsStream("/properties/skill_name.properties");
+			namePro.load(new InputStreamReader(in, "utf-8"));
+			in.close();
+
+			detailPro = new Properties();
+			in = ClassLoader.class.getResourceAsStream("/properties/skill_detail.properties");
+			detailPro.load(new InputStreamReader(in, "utf-8"));
+			in.close();
+
+			bookPro = new Properties();
+			in = ClassLoader.class.getResourceAsStream("/properties/skill_book.properties");
+			bookPro.load(new InputStreamReader(in, "utf-8"));
+			in.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	
+	private static void init() {
+		String xinfaNames = namePro.getProperty("xinfa");
+		Map<String, SkillDetail> xinfaMap = makeSkill(xinfaNames, true);
+		skillMap.put(SKILL_TYPE_XINFA, xinfaMap);
+
+		String quanzhangNames = namePro.getProperty("quanzhang");
+		Map<String, SkillDetail> quanzhangMap = makeSkill(quanzhangNames, false);
+		skillMap.put(SKILL_TYPE_QUANZHANG, quanzhangMap);
+
+		String zhifaNames = namePro.getProperty("zhifa");
+		Map<String, SkillDetail> zhifaMap = makeSkill(zhifaNames, false);
+		skillMap.put(SKILL_TYPE_ZHIFA, zhifaMap);
+
+		String jianfaNames = namePro.getProperty("jianfa");
+		Map<String, SkillDetail> jianfaMap = makeSkill(jianfaNames, false);
+		skillMap.put(SKILL_TYPE_JIANFA, jianfaMap);
+
+		String daofaNames = namePro.getProperty("daofa");
+		Map<String, SkillDetail> daofaMap = makeSkill(daofaNames, false);
+		skillMap.put(SKILL_TYPE_DAOFA, daofaMap);
+
+		String tuifaNames = namePro.getProperty("tuifa");
+		Map<String, SkillDetail> tuifaMap = makeSkill(tuifaNames, false);
+		skillMap.put(SKILL_TYPE_TUIFA, tuifaMap);
+	}
+	
+	private static Map<String, SkillDetail> makeSkill(String skillName, boolean isXinfa) {
+		Map<String, SkillDetail> xinfaMap = new HashMap<String, SkillDetail>();
+
+		String[] skillNames = skillName.split(",");
+		for (String sill : skillNames) {
+			SkillDetail skillDetail = new SkillDetail();
+			String detail = detailPro.getProperty(sill);
+			String[] details = detail.split(",");
+			if (isXinfa) {
+				skillDetail.setM_iNeigongID(Integer.parseInt(details[0]));
+				skillDetail.setM_strNeigongName(details[1]);
+				skillDetail.setM_strStatusImage(details[2]);
+				skillDetail.setM_strSelectImage(details[3]);
+			} else {
+				skillDetail.setM_iRoutineID(Integer.parseInt(details[0]));
+				skillDetail.setM_strRoutineName(details[1]);
+				skillDetail.setM_iWearAmsType(Integer.parseInt(details[2]));
+			}
+
+			String book = bookPro.getProperty(sill);
+			String[] books = book.split(",");
+			skillDetail.setM_iType(Integer.parseInt(books[0]));
+			skillDetail.setM_iID(Integer.parseInt(books[1]));
+			skillDetail.setM_iAbilityType(Integer.parseInt(books[2]));
+			skillDetail.setM_strAbilityID(books[3]);
+			skillDetail.setM_strBookMsg(books[4]);
+			skillDetail.setM_strBookImage(books[5]);
+			skillDetail.setM_sValueLink(books[6]);
+			xinfaMap.put(sill, skillDetail);
+		}
+
+		return xinfaMap;
+	}
+
+	public static Map<Integer, Map<String, SkillDetail>> getMap() {
+		return skillMap;
+	}
+	
+	public static void main(String[] args) {
+		Map<Integer, Map<String, SkillDetail>> m = SkillManager.getMap();
+		System.out.println(m);
+	}
+}
